41ce7698a9354f611d31394aab320c4acdb81ee5
patriciosandor <64619046+patriciosandor@users.noreply.github.com>
2020-08-19 07:49:27 -0300

    Desafio 1 de JS

---
+++ clase1.js
@@ -0,0 +1,11 @@
+var nombre = prompt ("Ingresar tu nombre");
+console.log("BONDIA " + nombre + " !!!!!!" + "que tengas un excelente dia ");
+
+var tuedad = prompt ("Ingresa tu edad");
+var unnumero = prompt ("Elegi un numero del 90 a 140");
+var edad =  parseInt (tuedad);
+var numeroal = parseInt (unnumero);
+
+var comidafavo = prompt ("Ingresa tu comida favorita ");
+var club = prompt ("Ingresa tu club de futbol ");
+alert ("BONDIA " + nombre + "FUTURO CRACK DE JS" + "\n2. Tenes " + tuedad + "\n3. Tu Peso dentro de 5 años es de " + (numeroal - edad) + "\n4 Tu comida favorita es " + comidafavo + "\n5 Y sos fan del " + club);
